04333f6038df92c81ae609e56509fe1a13da7b14
lukekh <luke.keating.hughes@gmail.com>
2019-11-26 22:09:06 +1030

    Reran the setup for package

---
+++ build/lib/groupy/__init__.py
@@ -0,0 +1,12 @@
+"""
+A Python library equipped with basic tools to build, sandbox and explore
+groupy theory and abstract algebraic objects.
+"""
+
+__author__ = "Luke Keating Hughes"
+
+__version__ = "0.0.1"
+
+
+__email__ = "groupython@gmail.com"
+
